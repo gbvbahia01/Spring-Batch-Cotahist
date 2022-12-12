597e434f72f636080351f020a054c3aea18b42e1
Guilherme Braga <gbvbahia01@gmail.com>
2022-12-12 13:55:53 -0300

    divide the values to 100.

--- src/main/java/br/com/gbvbahia/cotahist/batch/LineSetHeaderIdProcessor.java
+++ src/main/java/br/com/gbvbahia/cotahist/batch/LineSetHeaderIdProcessor.java
@@ -1,5 +1,7 @@
 package br.com.gbvbahia.cotahist.batch;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.annotation.BeforeStep;
@@ -19,6 +21,33 @@ public class LineSetHeaderIdProcessor implements ItemProcessor<Line, Line> {
   public Line process(Line item) throws Exception {
 
     item.setHeaderId(headerId);
+    if (item.getPreAbe() != null)
+      item.setPreAbe(item.getPreAbe().divide(new BigDecimal(100), 2, RoundingMode.HALF_UP));
+    
+    if (item.getPreMax() != null)
+      item.setPreMax(item.getPreMax().divide(new BigDecimal(100), 2, RoundingMode.HALF_UP));
+    
+    if (item.getPreMin() != null)
+      item.setPreMin(item.getPreMin().divide(new BigDecimal(100), 2, RoundingMode.HALF_UP));
+    
+    if (item.getPreMed() != null)
+      item.setPreMed(item.getPreMed().divide(new BigDecimal(100), 2, RoundingMode.HALF_UP));
+    
+    if (item.getPreUlt() != null)
+      item.setPreUlt(item.getPreUlt().divide(new BigDecimal(100), 2, RoundingMode.HALF_UP));
+    
+    if (item.getPreOfc() != null)
+      item.setPreOfc(item.getPreOfc().divide(new BigDecimal(100), 2, RoundingMode.HALF_UP));
+    
+    if (item.getPreOfv() != null)
+      item.setPreOfv(item.getPreOfv().divide(new BigDecimal(100), 2, RoundingMode.HALF_UP));
+    
+    if (item.getPreExe() != null)
+      item.setPreExe(item.getPreExe().divide(new BigDecimal(100), 2, RoundingMode.HALF_UP));
+    
+    if (item.getPtoExe() != null) 
+      item.setPtoExe(item.getPtoExe().divide(new BigDecimal(1000000), 2, RoundingMode.HALF_UP));
+    
     return item;
   }
 
--- src/test/java/br/com/gbvbahia/cotahist/CotahistB3ApplicationTests.java
+++ src/test/java/br/com/gbvbahia/cotahist/CotahistB3ApplicationTests.java
@@ -1,9 +1,7 @@
 package br.com.gbvbahia.cotahist;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-// @SpringBootTest
 class CotahistB3ApplicationTests {
 
   @Test
